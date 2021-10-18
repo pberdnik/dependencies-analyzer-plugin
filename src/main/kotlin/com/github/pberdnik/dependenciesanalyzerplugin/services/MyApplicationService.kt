56a292e2fa16d8f2647297ed17a60493b7e6f362
GitHub Action <action@github.com>
2021-10-18 16:38:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pberdnik/dependenciesanalyzerplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pberdnik.dependenciesanalyzerplugin.services
+
+import com.github.pberdnik.dependenciesanalyzerplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/pberdnik/dependenciesanalyzerplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pberdnik/dependenciesanalyzerplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pberdnik.dependenciesanalyzerplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pberdnik.dependenciesanalyzerplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
