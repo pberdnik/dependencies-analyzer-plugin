bd6a9aaf89ba3288734d795368a30513cc61f48b
Pavel Berdnikov <pberdnik@yandex-team.ru>
2021-10-20 21:06:20 +0300

    Add fake action

---
+++ src/main/kotlin/com/github/pberdnik/dependenciesanalyzerplugin/actions/PopupDialogAction.kt
@@ -0,0 +1,68 @@
+// Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.github.pberdnik.dependenciesanalyzerplugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import java.lang.StringBuilder
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.ui.Messages
+import javax.swing.Icon
+
+/**
+ * Action class to demonstrate how to interact with the IntelliJ Platform.
+ * The only action this class performs is to provide the user with a popup dialog as feedback.
+ * Typically this class is instantiated by the IntelliJ Platform framework based on declarations
+ * in the plugin.xml file. But when added at runtime this class is instantiated by an action group.
+ */
+class PopupDialogAction : AnAction {
+    /**
+     * This default constructor is used by the IntelliJ Platform framework to instantiate this class based on plugin.xml
+     * declarations. Only needed in [PopupDialogAction] class because a second constructor is overridden.
+     *
+     * @see AnAction
+     */
+    constructor() : super() {}
+
+    /**
+     * This constructor is used to support dynamically added menu actions.
+     * It sets the text, description to be displayed for the menu item.
+     * Otherwise, the default AnAction constructor is used by the IntelliJ Platform.
+     *
+     * @param text        The text to be displayed as a menu item.
+     * @param description The description of the menu item.
+     * @param icon        The icon to be used with the menu item.
+     */
+    constructor(text: String?, description: String?, icon: Icon?) : super(text, description, icon) {}
+
+    /**
+     * Gives the user feedback when the dynamic action menu is chosen.
+     * Pops a simple message dialog. See the psi_demo plugin for an
+     * example of how to use [AnActionEvent] to access data.
+     *
+     * @param event Event received when the associated menu item is chosen.
+     */
+    override fun actionPerformed(event: AnActionEvent) {
+        // Using the event, create and show a dialog
+        val currentProject = event.project
+        val dlgMsg = StringBuilder(event.presentation.text + " Selected!")
+        val dlgTitle = event.presentation.description
+        // If an element is selected in the editor, add info about it.
+        val nav = event.getData(CommonDataKeys.NAVIGATABLE)
+        if (nav != null) {
+            dlgMsg.append(String.format("\nSelected Element: %s", nav.toString()))
+        }
+        Messages.showMessageDialog(currentProject, dlgMsg.toString(), dlgTitle, Messages.getInformationIcon())
+    }
+
+    /**
+     * Determines whether this menu item is available for the current context.
+     * Requires a project to be open.
+     *
+     * @param e Event received when the associated group-id menu is chosen.
+     */
+    override fun update(e: AnActionEvent) {
+        // Set the availability based on whether a project is open
+        val project = e.project
+        e.presentation.isEnabledAndVisible = project != null
+    }
+}
